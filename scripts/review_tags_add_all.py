bd39ef7ead8f89633e3f5f1d1a2a5d9092cc4506
Anupam Mediratta <mediratta@gmail.com>
2015-09-08 23:35:42 +0530

    adding generator

--- scripts/review_tags_add_all.py
+++ scripts/review_tags_add_all.py
@@ -1,7 +1,9 @@
 import json
-import attribute_adjective_finder as finder
 import sys
 import re
+sys.path.insert(0, '/Volumes/anupam work/review-app-local/scripts/lib/')
+
+import language_functions as text_p
 
 def get_hotel_id(hotel_name, hotel_id_map):
     for key in hotel_id_map:
@@ -9,14 +11,28 @@ def get_hotel_id(hotel_name, hotel_id_map):
             return key
     return -1
 
-def get_review_details():
-    
+def get_review_details(attribute_seed, attribute_adjective_map, sent):
+    try:
+        path_with_score = text_p.find_attribute_2(attribute_seed, sent)
+        if path_with_score == None or path_with_score == []:
+            'path is none for line: ' + str(line)
+            yield None
+        #print 'path_with_score: ' + str(path_with_score)
+        path = map(lambda x: x[0], path_with_score)
+        score = map(lambda x: x[1], path_with_score)
+        #print 'score: ' + str(score)
+        cumulative_score = reduce(lambda x, y: x * y, score, 1.0)
+        adj_list = text_p.find_sentiment_adjective(attribute_adjective_map, path, sent)
+    except TypeError:
+        print 'Type Error in attribute_adjective_finder for line: ' + str(sent)
+        return
+    yield {'path': path, 'sentiment': sentiment, 'adj_list': adj_list }
 
 if __name__ == "__main__":
     attribute_seed = json.loads(open(sys.argv[1], 'r').read())
     attribute_adjective_map = json.loads(open(sys.argv[2], 'r').read())
     
-    finder.init()
+    text_p.load_for_adjectives()
     # read the data
     review_data = json.loads(open(sys.argv[3], 'r').read())
     hotel_id_map = json.loads(open(sys.argv[4], 'r').read())
@@ -38,21 +54,7 @@ if __name__ == "__main__":
             review_sentences = re.split('\.|\?| !', complete_review)
             result = []
             for sent in review_sentences:
-                try:
-                    path_with_score = text_p.find_attribute_2(attribute_seed, sent)
-                    if path_with_score == None or path_with_score == []:
-                        'path is none for line: ' + str(line)
-                        continue
-                    #print 'path_with_score: ' + str(path_with_score)
-                    path = map(lambda x: x[0], path_with_score)
-                    score = map(lambda x: x[1], path_with_score)
-                    #print 'score: ' + str(score)
-                    cumulative_score = reduce(lambda x, y: x * y, score, 1.0)
-                    adj_list = text_p.find_sentiment_adjective(attribute_adjective_map, path, sent)
-                except TypeError:
-                    print 'Type Error in attribute_adjective_finder for line: ' + str(sent)
-                    continue
-                result.append({'path': path, 'sentiment': sentiment, 'adj_list': adj_list })
+                result.append(get_review_details(attribute_seed, attribute_adjective_map, sent))
                 
             attr_to_insert = []
             sentiment_map = {}
