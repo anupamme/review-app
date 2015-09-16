1edf29a7d04c6d6f39797da795f6d81f68c56936
Anupam Mediratta <mediratta@gmail.com>
2015-09-16 16:25:09 +0530

    making city id another index.

--- scripts/review_tags_add_all_multi.py
+++ scripts/review_tags_add_all_multi.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     
     text_p.load_for_adjectives()
     # read the data
-    review_data = json.loads(open(sys.argv[3], 'r').read())
+    meta_review_data = json.loads(open(sys.argv[3], 'r').read())
     hotel_id_map = json.loads(open(sys.argv[4], 'r').read())
     output = []
     output_hotel_id_review_id = {}      #hotel_id -> review_id -> review_object
@@ -48,74 +48,79 @@ if __name__ == "__main__":
     
     pool = mp.Pool(processes=int(sys.argv[5]))
     
-    for val in review_data:
-        if hotel_count < hotels_so_far:
+    for city_id in meta_review_data:
+        review_data = meta_review_data[city_id]
+        output_hotel_id_review_id[city_id] = {}
+        for val in review_data:
+            if hotel_count < hotels_so_far:
+                hotel_count += 1
+                continue
+
             hotel_count += 1
-            continue
-        
-        hotel_count += 1
-        hotel_name = val['name']
-        hotel_id = get_hotel_id(hotel_name, hotel_id_map)
-        if hotel_id == -1:
-            print 'hotel_id not found for ' + hotel_name
-            hotel_id = random.randint(100, 1000)
-            #continue
-        output_hotel_id_review_id[hotel_id] = {}
-        reviews = val['reviews']
-        rev_id = 0
-        for rev in reviews:
-            output_hotel_id_review_id[hotel_id][rev_id] = rev
-            complete_review = ''.join(rev['description'])
-            review_sentences = re.split('\.|\?| !', complete_review)
-            #result = []
-            result = [pool.apply(get_review_details, args=(attribute_seed, attribute_adjective_map, x.encode('utf-8'),)) for x in review_sentences]
-#            for sent in review_sentences:
-#                sent = sent.encode('utf-8')
-#                item = get_review_details(attribute_seed, attribute_adjective_map, sent)
-#                
-#                if item != None:
-#                    result.append(item)
-            #print 'result: ' + str(result)
-            attr_to_insert = []
-            sentiment_map = {}
-            adj_list_map = {}
-            sentence_map = {}
-            sentence_count = 0
-            for obj in result:
-                if obj == None or obj['path'] == None or obj['path'] == []:
+            hotel_name = val['name']
+            hotel_id = get_hotel_id(hotel_name, hotel_id_map)
+            if hotel_id == -1:
+                print 'hotel_id not found for ' + hotel_name
+                hotel_id = random.randint(100, 1000)
+                #continue
+            output_hotel_id_review_id[city_id][hotel_id] = {}
+            reviews = val['reviews']
+            rev_id = 0
+            for rev in reviews:
+                output_hotel_id_review_id[city_id][hotel_id][rev_id] = rev
+                complete_review = ''.join(rev['description'])
+                review_sentences = re.split('\.|\?| !', complete_review)
+                #result = []
+                result = [pool.apply(get_review_details, args=(attribute_seed, attribute_adjective_map, x.encode('utf-8'),)) for x in review_sentences]
+    #            for sent in review_sentences:
+    #                sent = sent.encode('utf-8')
+    #                item = get_review_details(attribute_seed, attribute_adjective_map, sent)
+    #                
+    #                if item != None:
+    #                    result.append(item)
+                #print 'result: ' + str(result)
+                attr_to_insert = []
+                sentiment_map = {}
+                adj_list_map = {}
+                sentence_map = {}
+                sentence_count = 0
+                for obj in result:
+                    if obj == None or obj['path'] == None or obj['path'] == []:
+                        sentence_count += 1
+                        continue
+                    attr = obj['path'][len(obj['path']) - 1]
+                    attr_to_insert.append(attr)
+                    sentiment_map[attr] = obj['sentiment']
+                    adj_list_map[attr] = obj['adj_list']
+                    sentence_map[attr] = sentence_count
                     sentence_count += 1
-                    continue
-                attr = obj['path'][len(obj['path']) - 1]
-                attr_to_insert.append(attr)
-                sentiment_map[attr] = obj['sentiment']
-                adj_list_map[attr] = obj['adj_list']
-                sentence_map[attr] = sentence_count
-                sentence_count += 1
-            
-            attr_to_insert_2 = map(lambda x: {"value": x}, adj_list_map)
-            formed_object = {
-                "id": count,
-                "hotel_id": hotel_id,
-                "review_id": rev_id,
-                "attribute_list": attr_to_insert_2,
-                "sentiment": sentiment_map,
-                "adjective_list": adj_list_map,
-                "attribute_line": sentence_map
-            }
-            count += 1
-            rev_id += 1
-            output.append(formed_object)
-            
-            if count % 1000 == 0:
-                f = open('hotel_review_elastic_' + str(count) + '.json', 'w')
-                f.write(json.dumps(output))
-                f.close()
-                output = []
-                f = open('hotel_review_id_' + str(count) + '.json', 'w')
-                f.write(json.dumps(output_hotel_id_review_id))
-                f.close()
-                output_hotel_id_review_id = {}
-                output_hotel_id_review_id[hotel_id] = {}
+
+                attr_to_insert_2 = map(lambda x: {"value": x}, adj_list_map)
+                formed_object = {
+                    "city_id": city_id,
+                    "id": count,
+                    "hotel_id": hotel_id,
+                    "review_id": rev_id,
+                    "attribute_list": attr_to_insert_2,
+                    "sentiment": sentiment_map,
+                    "adjective_list": adj_list_map,
+                    "attribute_line": sentence_map
+                }
+                count += 1
+                rev_id += 1
+                output.append(formed_object)
+
+                if count % 1000 == 0:
+                    f = open('hotel_review_elastic_' + str(count) + '.json', 'w')
+                    f.write(json.dumps(output))
+                    f.close()
+                    output = []
+                    f = open('hotel_review_id_' + str(count) + '.json', 'w')
+                    f.write(json.dumps(output_hotel_id_review_id))
+                    f.close()
+                    output_hotel_id_review_id = {}
+                    output_hotel_id_review_id[city_id] = {}
+                    output_hotel_id_review_id[city_id][hotel_id] = {}
     
     
     f = open('hotel_review_id.json', 'w')
